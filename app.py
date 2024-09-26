46640684f2c8c311c7bc1beee466b74d42fa07f2
4T4L4Y <81240785+4T4L4Y@users.noreply.github.com>
2024-09-26 11:25:12 +0300

    a

--- app.py
+++ app.py
@@ -1,116 +1,170 @@
-from flask import Flask, render_template, request
-import os
+from flask import Flask, render_template, request, send_file, jsonify
+import os,json,re
 
+OUTPUTS_DIR = "outputs"
+if not os.path.exists(OUTPUTS_DIR):
+    os.makedirs(OUTPUTS_DIR)
 
-subdomain_scan_done = False
 app = Flask(__name__)
 
-def run_naabu(domain):
-    command = f"naabu -host {domain} -o {domain}_naabu.txt"
+def run_naabu(domain, ports):
+    if ports:
+        command = f"naabu -host {domain} -p {ports} -o {OUTPUTS_DIR}/{domain}_naabu.txt"
+    else:
+        command = f"naabu -host {domain} -o {OUTPUTS_DIR}/{domain}_naabu.txt"
+    print(command)
     os.system(command)
     print("[+] Naabu scan completed.")
 
-
-def find_subdomains(domain):
-    global subdomain_scan_done
-    print("[+] Initiating subdomain scan...")
-    output_file = f"{domain}_subdomains.txt"
-
+def find_subdomains(domain, params):
+    output_file = f"{OUTPUTS_DIR}/{domain}_subdomains.txt"
     if os.path.exists(output_file):
         os.remove(output_file)
-
-    command = f"subfinder -d {domain} -o {domain}_subdomains.txt"
+    params_str = " ".join(params)
+    command = f"subfinder -d {domain} {params_str} -o {output_file}"
+    print(command)
     os.system(command)
     print("[+] Subdomain scan completed.")
-    subdomain_scan_done = True
 
 
-def wayback_urls(domain):
-    if subdomain_scan_done:
-        # If subdomain scan has been done
-        with open(f"{domain}_subdomains.txt", 'r') as f:
-            for subdomain in f.readlines():
-                subdomain = subdomain.strip()
-                if not os.path.exists(f"{subdomain}_wayback.txt"):
-                    command = f"waybackpy -u {subdomain} --known_urls > {subdomain}_wayback.txt"
-                    os.system(command)
-    else:
-        # If subdomain scan has not been done or if the wayback file doesn't exist
-        if not os.path.exists(f"{domain}_subdomains.txt"):
-            command = f"waybackpy -u {domain} --known_urls > {domain}_wayback.txt"
-            os.system(command)
-            print("[+] URL collection with Wayback Machine completed.")
+def wayback_urls(domain, known_urls=False, include_subdomains=False):
+    command_suffix = " "
+    if known_urls:
+        command_suffix += " --known_urls"
+    if include_subdomains:
+        command_suffix += " --subomains"
+    with open(f"{OUTPUTS_DIR}/{domain}_subdomains.txt", 'r') as f:
+        for subdomain in f.readlines():
+            subdomain = subdomain.strip()
+            output_file = f"{OUTPUTS_DIR}/{subdomain}_wayback.txt"
+            if not os.path.exists(output_file):
+                command = f"waybackpy -u {subdomain}{command_suffix} > {output_file}"
+                print(command)
+                os.system(command)
 
 
 
-def run_httpx(domain):
+def run_httpx(domain, follow_redirects=False, show_status=False, match_codes=""):
     print("[+] Initiating URL scan with HTTPX...")
-    wayback_files = [f for f in os.listdir() if f.endswith(f"{domain}_wayback.txt")]
+    wayback_files = [f for f in os.listdir(OUTPUTS_DIR) if f.endswith(f"{domain}_wayback.txt")]
+
+    base_command = "httpx -nc"
+
+    if follow_redirects:
+        base_command += " -fr"
+
+    if show_status:
+        base_command += " -sc"
+
+    if match_codes:
+        base_command += f" -mc {match_codes}"
 
     if wayback_files:
         for wayback_file in wayback_files:
-            command = f"httpx -l {wayback_file} -tech-detect  -fr -nc -sc -mc 200,302,400,401,403,404,500 -o {wayback_file.replace('_wayback.txt', '_httpx.txt')}"
+            output_file = f"{OUTPUTS_DIR}/{wayback_file.replace('_wayback.txt', '_httpx.txt')}"
+            command = f"{base_command} -l {OUTPUTS_DIR}/{wayback_file} -o {output_file}"
             os.system(command)
     else:
-        command = f"httpx -target {domain} -fr -nc -sc -mc 200,302,400,401,403,404,500 -o {domain}_httpx.txt"
+        output_file = f"{OUTPUTS_DIR}/{domain}_httpx.txt"
+        command = f"{base_command} -target {domain} -o {output_file}"
+        print(command)
         os.system(command)
 
     print("[+] URL scan with HTTPX completed.")
-
-def nuclei_scan(domain):
+def nuclei_scan(domain, raw_url, templates, severities):
     print("[+] Initiating Nuclei scan...")
-    command = f"nuclei -u {domain} -nh -nc -o={domain}_nuclei.txt "
+    template_ids = templates.split(",")
+    templates_str = " ".join(f"-id {template}" for template in template_ids) if templates else ""
+    severity_str = "-severity " + ",".join(severities) if severities else ""
+    output_txt = f"{OUTPUTS_DIR}/{domain}_nuclei.txt"
+    output_json = f"{OUTPUTS_DIR}/{domain}_nuclei.json"
+    command = f"nuclei -u {raw_url} {templates_str} {severity_str} -nh -nc -o {output_txt} -je {output_json}"
+    print(command)
     os.system(command)
     print("[+] Nuclei scan completed.")
+    return output_txt
+
+def read_and_combine_results(directory, domain, file_suffix):
+    combined_output = ""
+    files = [f for f in os.listdir(directory) if f.endswith(file_suffix) and domain in f]
+    for file in files:
+        with open(os.path.join(directory, file), "r") as file_content:
+            combined_output += file_content.read()
+    return combined_output
+
+def extract_domain(url):
+    domain_regex = r"^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)"
+    match = re.match(domain_regex, url)
+    if match:
+        return match.group(1)
+    return url
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    domain = ""
+    naabu_output = ""
     subdomain_output = ""
     wayback_output = ""
     httpx_output = ""
-    naabu_output = ""
     nuclei_output = ""
 
+
     if request.method == "POST":
-        domain = request.form.get("domain")
+        raw_url = request.form.get("domain")
+        domain = extract_domain(raw_url)
         choices = request.form.getlist("choices")
 
-        for choice in choices:
-            choice = choice.strip()
-            if choice == "1":
-                run_naabu(domain)
-                naabu_files = [f for f in os.listdir() if f.endswith("_naabu.txt") and domain in f]
-                for naabu_file in naabu_files:
-                    with open(naabu_file, "r") as naabu_file_content:
-                        naabu_output += naabu_file_content.read()
-            elif choice == "2":
-                find_subdomains(domain)
-                subdomain_files = [f for f in os.listdir() if f.endswith("_subdomains.txt") and domain in f]
-                for subdomain_file in subdomain_files:
-                    with open(subdomain_file, "r") as subdomain_file_content:
-                        subdomain_output += subdomain_file_content.read()
-            elif choice == "3":
-                wayback_urls(domain)
-                wayback_files = [f for f in os.listdir() if f.endswith("_wayback.txt") and domain in f]
-                for wayback_file in wayback_files:
-                    with open(wayback_file, "r") as wayback_file_content:
-                        wayback_output += wayback_file_content.read()
-            elif choice == "4":
-                run_httpx(domain)
-                httpx_files = [f for f in os.listdir() if f.endswith("_httpx.txt") and domain in f]
-                for httpx_file in httpx_files:
-                    with open(httpx_file, "r") as httpx_files_content:
-                        httpx_output += httpx_files_content.read()
-            elif choice == "5":
-                nuclei_scan(domain)
-                nuclei_files = [f for f in os.listdir() if f.endswith("_nuclei.txt") and domain in f]
-                for nuclei_file in nuclei_files:
-                    with open(nuclei_file,"r") as nuclei_files_content:
-                        nuclei_output += nuclei_files_content.read()
+        if '1' in choices:
+            ports = request.form.get("naabuPorts", "")
+            run_naabu(domain, ports)
+            naabu_output = read_and_combine_results(OUTPUTS_DIR, domain, "_naabu.txt")
+
+        if '2' in choices:
+            subdomain_params = request.form.get("subdomainParams", "").split()  # Liste haline getir
+            find_subdomains(domain, subdomain_params)
+            subdomain_output = read_and_combine_results(OUTPUTS_DIR, domain, "_subdomains.txt")
+
+        if '3' in choices:
+            known_urls = request.form.get('waybackKnownUrls') == 'on'
+            include_subdomains = request.form.get('includeSubdomains') == 'on'
+            wayback_urls(domain, known_urls, include_subdomains)
+            wayback_output = read_and_combine_results(OUTPUTS_DIR, domain, "_wayback.txt")
+
+        if '4' in choices:
+            follow_redirects = request.form.get('httpxFollowRedirects') == 'on'
+            show_status = request.form.get('httpxStatusCode') == 'on'
+            match_codes = request.form.get('httpxMatchCodes', '').replace(" ", "")
+            run_httpx(domain, follow_redirects, show_status, match_codes)
+            httpx_output = read_and_combine_results(OUTPUTS_DIR, domain, "_httpx.txt")
+
+        if '5' in choices:
+            nuclei_templates = request.form.get("nucleiTemplates", "")
+            nuclei_severities = request.form.getlist("nucleiSeverity")
+            nuclei_scan(domain, raw_url , nuclei_templates, nuclei_severities)
+            nuclei_output = read_and_combine_results(OUTPUTS_DIR, domain, "_nuclei.txt")
 
 
-    return render_template("index.html", naabu_output=naabu_output, subdomain_output=subdomain_output, wayback_output=wayback_output, httpx_output=httpx_output, nuclei_output=nuclei_output)
+    return render_template("index.html", domain=domain, naabu_output=naabu_output,
+                           subdomain_output=subdomain_output, wayback_output=wayback_output,
+                           httpx_output=httpx_output, nuclei_output=nuclei_output)
 
+@app.route("/outputs/<path:filename>")
+def download_file(filename):
+    full_path = os.path.join(app.root_path,OUTPUTS_DIR,  filename)
+    if os.path.exists(full_path):
+        return send_file(full_path, as_attachment=True)
+    else:
+        return "Dosya Yok" , 404
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
+
+@app.route("/outputs/<domain>")
+def nuclei_data(domain):
+    path = os.path.join(app.root_path, OUTPUTS_DIR, f"{domain}_nuclei.json")
+    try:
+        with open(path, 'r') as file:
+            data = json.load(file)
+            return jsonify(data)
+    except FileNotFoundError:
+        return jsonify({"error": "File not found"}), 404
